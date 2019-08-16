507e36ba87c228e79787ddaa6110edcfdb0fe2c6
Artem Prymostka <artemp.mail@gmail.com>
2019-08-16 21:27:33 +0300

    Updates

---
+++ precache-manifest.649c2302d2f339d278e983b934619e09.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669cedd0fa16b05b30a95f4fd8df3a4b",
+    "url": "/listen-to-the-radio/index.html"
+  },
+  {
+    "revision": "258a7aacb9afd7fa86f6",
+    "url": "/listen-to-the-radio/static/css/main.e629729f.chunk.css"
+  },
+  {
+    "revision": "d5a027ee380ee49d6799",
+    "url": "/listen-to-the-radio/static/js/2.be11bcac.chunk.js"
+  },
+  {
+    "revision": "258a7aacb9afd7fa86f6",
+    "url": "/listen-to-the-radio/static/js/main.43032c55.chunk.js"
+  },
+  {
+    "revision": "0a641bb7c7a109d76db1",
+    "url": "/listen-to-the-radio/static/js/runtime~main.d24a8c14.js"
+  },
+  {
+    "revision": "2c5a2681e5ed5df6c5e423d18e745260",
+    "url": "/listen-to-the-radio/static/media/no-img.2c5a2681.svg"
+  }
+]);
